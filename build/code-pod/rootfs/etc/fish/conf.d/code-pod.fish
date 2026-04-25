a38dffd1e51f66d7625d71bd50c56f31db839acd
Jonathan Senecal <contact@jonathansenecal.com>
2026-04-25 15:31:59 -0400

    feat(code-pod): add sudoers, fish, profile env drop-ins

---
+++ build/code-pod/rootfs/etc/fish/conf.d/code-pod.fish
@@ -0,0 +1,4 @@
+set -gx EDITOR vim
+set -gx PAGER less
+set -gx LESS '-R'
+fish_add_path /usr/local/bin /usr/local/sbin
---
+++ build/code-pod/rootfs/etc/profile.d/code-pod.sh
@@ -0,0 +1,8 @@
+export EDITOR=vim
+export PAGER=less
+export LESS='-R'
+export NPM_CONFIG_PREFIX=/usr/local
+case ":$PATH:" in
+    *":/usr/local/bin:"*) ;;
+    *) export PATH="/usr/local/bin:/usr/local/sbin:$PATH" ;;
+esac
